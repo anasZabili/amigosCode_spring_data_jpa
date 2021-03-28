921af119f78ccb8904d12a73b8f813ca372c4fbc
anas zabili <anaszab78@gmail.com>
2021-03-28 23:31:14 +0200

    @colum (permet d'ajouter tout sorte d'attribut et de contrainte a nos colonne)

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,10 +19,22 @@ public class Student {
   // l'alocation size est la pas d'increment en chaque nouveau id
   @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+  // column permet de specifier le name exact de la colonne de la bdd
+  // et d'autre contrainte appliqué sur une colone
+  @Column(name = "id", updatable = false)
   private Long id;
+
+  // columnDefinition nous permet de spécifier le type des data de la column
+  @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
   private String firstName;
+
+  @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
   private String lastName;
+
+  @Column(name = "email", nullable = false, columnDefinition = "TEXT", unique = true)
   private String email;
+
+  @Column(name = "age", nullable = false)
   private Integer age;
 
   public Student(Long id, String firstName, String lastName, String email, Integer age) {
